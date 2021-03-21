868b49c5595f5e7fa06661238b50fa9ffdcbfa78
accik <akselimk97@gmail.com>
2021-03-21 14:12:36 +0200

    suurempia juttuja

--- progressbar.py
+++ progressbar.py
@@ -1,40 +1,37 @@
 import time
 import sys
 
-def p_bar(total):
+'''
+From https://gist.github.com/sibosutd/c1d9ef01d38630750a1d1fe05c367eb8
+Modified by accik
+Version 0.2
+'''
+
+def p_bar(total, i_text): # Whole bar
+    if i_text == "":
+        text = "Completed"
+    else:
+        text = i_text
     bar_length = 30  # should be less than 100
     for i in range(total+1):
         percent = 100.0*i/total
         sys.stdout.write('\r')
-        sys.stdout.write("Completed: [{:{}}] {:>3}%"
-                        .format('='*int(percent/(100.0/bar_length)),
+        sys.stdout.write("{:}: [{:{}}] {:>3}%"
+                        .format(text,'='*int(percent/(100.0/bar_length)),
                                 bar_length, int(percent)))
         sys.stdout.flush()
         time.sleep(0.005)
+    return None
 
-def progress_bar2(total, i):
-
+def progress_bar2(total, i): # One i at the time
     bar_length = 30  # should be less than 100
     percent = 100.0*i/total
     sys.stdout.write('\r')
-    sys.stdout.write("Completed: [{:{}}] {:>3}%"
+    sys.stdout.write("Completed: [{:{}}] {:>3}% "
                     .format('='*int(percent/(100.0/bar_length)),
                                 bar_length, int(percent)))
     sys.stdout.flush()
     time.sleep(0.005)
+    return None
 
-# total = 2000  # total number to reach
-# bar_length = 30  # should be less than 100
-# for i in range(total+1):
-#     percent = 100.0*i/total
-#     sys.stdout.write('\r')
-#     sys.stdout.write("Completed: [{:{}}] {:>3}%"
-#                      .format('='*int(percent/(100.0/bar_length)),
-#                              bar_length, int(percent)))
-#     sys.stdout.flush()
-#     time.sleep(0.002)
-#     # time.sleep(5)
-
-# p_bar(50)
-
-# p_bar2(50)
+# print(testi().y)
