51aac9042476043a11dde5a692b1f1491b092bb4
accik <akselimk97@gmail.com>
2021-04-30 17:18:17 +0300

    small updates

--- url_to_html.py
+++ url_to_html.py
@@ -1,7 +1,7 @@
 from urllib.request import urlopen # To open URL provided
 import urllib.request # To make the request
 from urllib.error import HTTPError
-import sys # Needed?
+import sys # Keeping for now
 
 class bcolors: # Grabbed from https://stackoverflow.com/questions/287871/how-to-print-colored-text-to-the-terminal
     # These should work on every terminal with python3+
@@ -13,7 +13,6 @@ class settings():
     user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:85.0) Gecko/20100101 Firefox/85.0" # Use what user agent you want, new one to be sure
     headers = {'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8','User-Agent': user_agent} # header from Google :P
     debug = 0
-
 
 def get_html(url, *args):
     try:
@@ -37,6 +36,8 @@ def get_html(url, *args):
             print(f"{bcolors.WARNING}HTTP Error 403 forbidden, most likely getting rate limited{bcolors.ENDC}")
         else:
             raise
+        return err.code # Returns if any error happens
     except Exception:
         print(f"{bcolors.FAIL}Cannot access webpage{bcolors.ENDC}") # If we get timed out or other issues
-    return html
+        # sys.exit(0) # Not sure
+    return html # If no errors
